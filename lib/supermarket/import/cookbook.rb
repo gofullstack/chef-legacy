68a40896beed2971acd280cab83de691655fb7ce
Brian Cobb <bcobb@uwalumni.com>
2014-05-07 13:44:51 -0500

    Import only the most recent of a given cookbook version
    This protects against the handful of instances where Rails's built-in
    uniqueness validation failed on the existing community site. It matches
    the API behavior of the existing community site, which, when showing one
    of these duplicate versions, shows the most recently-uploaded version.

--- lib/supermarket/import/cookbook.rb
+++ lib/supermarket/import/cookbook.rb
@@ -16,23 +16,33 @@ module Supermarket
       end
 
       def call
-        cookbook_versions = @record.cookbook_versions.map do |record|
-          ::CookbookVersion.new(
-            description: @record.description,
-            version: record.version,
-            license: record.license,
-            tarball_file_name: record.tarball_file_name,
-            tarball_content_type: record.tarball_content_type,
-            tarball_file_size: record.tarball_file_size,
-            tarball_updated_at: record.tarball_updated_at,
-            download_count: record.download_count,
-            created_at: record.created_at,
-            updated_at: record.updated_at,
-            legacy_id: record.id
-          ).tap do |cookbook_version|
-            cookbook_version.record_timestamps = false
-            cookbook_version.save!
-          end
+        cookbook_versions = @record.cookbook_versions.
+          group_by(&:version).
+          map do |_, records|
+            # NOTE: as of writing, there are eight cookbooks which have
+            # multiple associated CookbookVersion records with the same version
+            # number (there is Rails validation against this, but no database
+            # constriants). Supermarket's database constraints enforce
+            # uniqueness in this regard, so we take the most recently-uploaded
+            # version as the version to import
+            record = records.max_by(&:created_at)
+
+            ::CookbookVersion.new(
+              description: @record.description,
+              version: record.version,
+              license: record.license,
+              tarball_file_name: record.tarball_file_name,
+              tarball_content_type: record.tarball_content_type,
+              tarball_file_size: record.tarball_file_size,
+              tarball_updated_at: record.tarball_updated_at,
+              download_count: record.download_count,
+              created_at: record.created_at,
+              updated_at: record.updated_at,
+              legacy_id: record.id
+            ).tap do |cookbook_version|
+              cookbook_version.record_timestamps = false
+              cookbook_version.save!
+            end
         end
 
         category = ::Category.with_name(@record.category.name).first!
